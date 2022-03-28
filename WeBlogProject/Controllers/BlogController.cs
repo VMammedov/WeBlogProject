a865f27130e7d293db0a90cd302952c50494ff44
VMammedov <vusal.memmedli25official@gmail.com>
2022-03-28 11:29:47 +0400

    Initialize

---
+++ WeBlogProject/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeBlogProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new BlogRepository());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
